807e5f4f8ed2817f719855c39715ece2ed125ec6
Adil Hussain <adilson05uk@gmail.com>
2021-03-19 19:01:44 +0000

    Added `XCUIElementQuery` extension.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "XCTestExtensions",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_11), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
---
+++ Sources/XCTestExtensions/XCUIElementQueryExtension.swift
@@ -0,0 +1,22 @@
+//
+//  XCUIElementQueryExtension.swift
+//  HifdhTrackerUITests
+//
+//  Created by Adil Hussain on 19/03/2021.
+//  Copyright © 2021 Tazkiya Tech. All rights reserved.
+//
+
+import XCTest
+
+extension XCUIElementQuery {
+    
+    /**
+     * Asserts that the number of elements that match this query is `count`.
+     *
+     * # Reference
+     * [XCUIElementQuery.count](https://developer.apple.com/documentation/xctest/xcuielementquery/1500625-count)
+     */
+    public func assertCount(count: Int) {
+        XCTAssertEqual(count, self.count)
+    }
+}
